87d42658f0257b4abdb76640f69928d5fe479fa8
boefraty74 <boefraty@microsoft.com>
2017-04-17 15:55:11 +0300

    temp dev 001

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var PBI_CV_AD348DC9_41C4_4867_B19O_53F9RDC6IEAS_HTML = {
+        name: 'PBI_CV_AD348DC9_41C4_4867_B19O_53F9RDC6IEAS_HTML',
+        displayName: 'Forecasting with ARIMA',
+        class: 'Visual',
+        version: '1.0.1',
+        apiVersion: '1.4.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.PBI_CV_AD348DC9_41C4_4867_B19O_53F9RDC6IEAS_HTML.Visual(options),
+        custom: true
+    };
+}
